b45a5eaec6d49bf9931395d285da53c24a114382
sumin305 <gogotnals@naver.com>
2023-08-03 15:24:30 +0900

    fix: PR 반영 1. 버튼 명명 변경 2. 버튼 State enum type추가 3. 버튼 State Binding으로 받아오기

---
+++ iOS/Catalog/Catalog/Component/BottomButton/CLButtonState.swift
@@ -0,0 +1,15 @@
+//
+//  ButtonState.swift
+//  Catalog
+//
+//  Created by 이수민 on 2023/08/03.
+//
+
+import Foundation
+
+enum CLButtonState {
+    case `default`
+    case isClicked
+    case inActive
+}
+
