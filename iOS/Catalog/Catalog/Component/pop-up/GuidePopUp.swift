e443f8530ca99437ac95a3573941d52c64650a23
sumin305 <gogotnals@naver.com>
2023-08-03 18:44:50 +0900

    feat: guidePopup mid-testing

--- iOS/Catalog/Catalog/Component/pop-up/GuidePopUp.swift
+++ iOS/Catalog/Catalog/Component/pop-up/GuidePopUp.swift
@@ -13,35 +13,43 @@ struct GuidePopUp: View {
     
     let width: CGFloat
     let height: CGFloat
-    let title: String
+    let title: AttributedString
+    var accentText: String = ""
     let description: String
     
     var body: some View {
         ZStack {
             Color("dimBackground").opacity(0.5).ignoresSafeArea()
-            Image(rectangleImage)
             VStack {
-                ZStack(alignment: .top) {
-                    HStack(alignment: .top) {
-                        Text(title).catalogFont(type: .HeadKRMedium14).foregroundColor(.gray900
-                        )
-                        Spacer()
-                    }
-                    HStack(alignment: .top) {
-                        Spacer()
-                        Button {
-                            showPopUp = false
-                        } label: {
-                            Image("24px icon")
+                Spacer()
+                ZStack {
+                    Image(rectangleImage)
+                    VStack {
+                        ZStack(alignment: .top) {
+                            HStack(alignment: .top) {
+                                Text(attributedString).catalogFont(type: .HeadKRMedium14)
+                                Spacer()
+                            }
+                            HStack(alignment: .top) {
+                                Spacer()
+                                Button {
+                                    showPopUp = false
+                                } label: {
+                                    Image("24px icon")
+                                }
+                            }
                         }
-                    }
+                        HStack {
+                            Divider().frame(width: 36, height: 1).background(Color.primary200)
+                            Spacer()
+                        }
+                        Text(description).catalogFont(type: .TextKRRegular12).foregroundColor(Color.primary500)
+                    }.frame(maxWidth: width, maxHeight: height).padding(.vertical, 10)
                 }
-                HStack {
-                    Divider().frame(width: 36, height: 1).background(Color.primary200)
-                    Spacer()
-                }
-                Text(description).catalogFont(type: .TextKRRegular12).foregroundColor(Color.primary500)
-            }.frame(maxWidth: width).padding(.vertical, 0.5)
+                Spacer()
+                Spacer()
+            }
+            
         }
     }
 }
@@ -54,6 +62,18 @@ struct GuidePopUp_Previews: PreviewProvider {
                    width: 214,
                    height: 236,
                    title: "현대자동차만이\n제공하는 실활용 데이터로\n합리적인 차량을 만들어 보세요.",
+                   accentText: "실활용 데이터",
                    description: "HMG Data 마크는 Hyundai Motor Group\n에서만 제공하는 데이터입니다.\n주행 중 운전자들이 실제로 얼마나 활용하는지를\n 추적해 수치화한 데이터 입니다.")
+    }
+}
+
+extension GuidePopUp {
+    @available(iOS 15, *)
+    var attributedString: AttributedString {
+        var text = title
+        var range = text.range(of: accentText)!
+        text.foregroundColor = Color.gray900
+        text[range].foregroundColor = Color.activeBlue
+        return text
     }
 }
