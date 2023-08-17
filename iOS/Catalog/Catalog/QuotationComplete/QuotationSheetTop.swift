4295be9f1c9d8f54eb366647cae6259f196ca5c7
이수민 <gogotnals@naver.com>
2023-08-17 10:31:12 +0900

    feat: RoundedRectangle -> Rectangle 변경

--- iOS/Catalog/Catalog/QuotationComplete/QuotationSheetTop.swift
+++ iOS/Catalog/Catalog/QuotationComplete/QuotationSheetTop.swift
@@ -18,8 +18,9 @@ struct QuotationSheetTop: View {
       }
       .frame(maxWidth: .infinity, maxHeight: 30)
       .background(
-        RoundedRectangle(cornerRadius: 20)
+        Rectangle()
           .fill(.white)
+          .cornerRadius(20, corners: [.topLeft, .topRight])
           .shadow(radius: 1, x: 0, y: -1)
       )
     }
@@ -28,5 +29,22 @@ struct QuotationSheetTop: View {
 struct QuotationSheetTop_Previews: PreviewProvider {
     static var previews: some View {
         QuotationSheetTop()
+    }
+}
+
+extension View {
+    func cornerRadius(_ radius: CGFloat, corners: UIRectCorner) -> some View {
+        clipShape( RoundedCorner(radius: radius, corners: corners) )
+    }
+}
+
+struct RoundedCorner: Shape {
+
+    var radius: CGFloat = .infinity
+    var corners: UIRectCorner = .allCorners
+
+    func path(in rect: CGRect) -> Path {
+        let path = UIBezierPath(roundedRect: rect, byRoundingCorners: corners, cornerRadii: CGSize(width: radius, height: radius))
+        return Path(path.cgPath)
     }
 }
