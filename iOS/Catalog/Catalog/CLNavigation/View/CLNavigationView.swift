130266ea9c0d318f124e4b4d06b312048ad57db9
이수민 <gogotnals@naver.com>
2023-08-17 16:14:16 +0900

    feat: 리뷰 반영 및 견적 완료 페이지 시트 생성

--- iOS/Catalog/Catalog/CLNavigation/View/CLNavigationView.swift
+++ iOS/Catalog/Catalog/CLNavigation/View/CLNavigationView.swift
@@ -111,3 +111,13 @@ struct CLNavigationView_Previews: PreviewProvider {
     return CLNavigationView.build(intent: CLNavigationIntent(initialState: .init(currentPage: 0)))
   }
 }
+
+extension ShapeStyle where Self == Color {
+  static var random: Color {
+    Color(
+      red: .random(in: 0...1),
+      green: .random(in: 0...1),
+      blue: .random(in: 0...1)
+    )
+  }
+}
