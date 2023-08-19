4f4023199df5b23f168b0e770dee31f7720cccdb
sumin305 <gogotnals@naver.com>
2023-08-20 01:53:17 +0900

    feat: SimilarQuotationCard 구현2
    사진, 상세 정보

--- iOS/Catalog/Catalog/SimilarQuotation/Model/SimilarQuotationModel.swift
+++ iOS/Catalog/Catalog/SimilarQuotation/Model/SimilarQuotationModel.swift
@@ -17,7 +17,7 @@ enum SimilarQuotationModel {
   }
   enum ViewAction: Equatable {
     case onAppear(quotation: CarQuotation)
-    case optionSelected(selectedOptionId: SimilarQuotationOption)
+    case optionSelected(selectedOption: SimilarQuotationOption)
     case onTapAddButton
     case onTapBackButton
   }
